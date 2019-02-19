fe2d6a5ca1cfba05f6cb69f33a3c325a3e6acc1b
Jordan Carlson <jordancarlson08@gmail.com>
2019-02-19 08:38:58 -0700

    make brightness app more flexible

--- apps/brightness.py
+++ apps/brightness.py
@@ -4,7 +4,10 @@ import appdaemon.plugins.hass.hassapi as hass
 # Set automatic brightness level at certain times
 #
 # Args:
-#   entity_id: entity ID of the light
+#   entity_id: entity Id of the light
+#   brightness: level between 0-255
+#   start: time ex "21:00:00"
+#   stop: time ex "06:00:00"
 #
 class Brightness(hass.Hass):
 
@@ -12,5 +15,5 @@ class Brightness(hass.Hass):
         self.listen_state(self.lights_on, self.args["entity_id"], new = "on", old="off")
 
     def lights_on(self, entity, attribute, old, new, kwargs):
-        if self.now_is_between("21:00:00", "06:00:00"):
-            self.turn_on(self.args["entity_id"], brightness = 20)
+        if self.now_is_between(self.args["start"], self.args["stop"]):
+            self.turn_on(self.args["entity_id"], brightness = self.args["brightness"])
